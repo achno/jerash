02fe01905d1b3606eb73128b7206c3e61ea5b927
Chico Camargo <achno@users.noreply.github.com>
2018-01-21 15:56:20 +0000

    Added preliminary simulation: one slider, two bars
    Added images for peep and tent,
    and made a preliminary simulation with one slider and two bars that grow from 0 to 100%.

--- sample.js
+++ sample.js
@@ -1,22 +1,112 @@
-var rSlider, xSlider;
+var button;
+var pSlider;
 
-function setup() {
-  // create canvas
-  createCanvas(windowWidth, windowHeight);
-  textFont("sans",30);
-  fill(242, 230, 213);
+// Simulation variables
+var pile1, pile2;
+var time, timemax;
+var p;
 
-  var r0 = 600;
-  var x0 = 100;
-  var dt0 = 25;
 
-  // create sliders
-  rSlider = createSlider(0, 800, r0);
-  rSlider.position(100, 100);
-  //xSlider = createSlider(0, 1000, x0);
-  //xSlider.position(100, 160);
-  //tSlider = createSlider(0, 100, dt0);
-  ///tSlider.position(100, 220);
+function setup()
+{
+    // create canvas
+    createCanvas(windowWidth, windowHeight);
+    textFont("sans",30);
+    fill(250);
+
+    // load images
+    column = loadImage("img/column.svg");
+    tent   = loadImage("img/tent.svg");
+    peep   = loadImage("img/peep.png");
+
+    // create button
+    var col = color(242,230,213,50);
+    button = createButton('&#9658; PLAY');
+    button.style('background-color',col)
+    button.style('font-size','25px');
+    button.mousePressed(run_simulation);
+    button.position(425,87);
+
+    // create slider
+    pSlider = createSlider(0, 100, 50);
+    pSlider.position(100, 100);
+
+    // setup simulation variables
+    pA = 0;
+    pB = 0;
+    timemax = 5000;
+    time = -1;
+}
+
+
+function run_simulation()
+{
+    p = pSlider.value();
+    reset_simulation();
+}
+
+function reset_simulation()
+{
+    pA = 0; pB = 0; time = 0;
+}
+
+function update_simulation()
+{
+    if( ( time >= 0 ) && (time < timemax) )
+    {
+        for(dt=0; dt<timemax/100; dt++)
+        {
+            var r = random(100);
+            if( r > p )
+            {
+                pA += 1;
+            }
+            else
+            {
+                pB += 1;
+            }
+            time += 1;
+        }
+        //console.log(pA,pB);
+    }
+}
+
+function draw_simulation(x, y)
+{
+    var c_stroke;
+    if( ( time > 0 ) && (time <= timemax) )
+    {
+        push();
+            strokeWeight(4);
+            translate(x,y);
+            height = 263;
+
+            var fa = height*pA/timemax
+            var fb = height*pB/timemax
+            push();
+                c_stroke = color(0,204,255);
+                stroke(c_stroke);
+                fill(color('#79D5FB'));
+                rect(100,200,50,-fa);
+                
+                noStroke();
+                fill(c_stroke);
+                percent_a = round(pA/(pA+pB)*100)
+                text(percent_a+"%", 100, 200-fa-7);
+            pop();
+            push();
+                c_stroke = color(255,204,0);
+                stroke(c_stroke);
+                fill(color('#FFE788'));
+                rect(200,200,50,-fb);
+                
+                noStroke();
+                fill(c_stroke);
+                percent_b = round(pB/(pA+pB)*100)
+                text(percent_b+"%", 200, 200-fb-7);
+            pop();
+        pop();
+    }
 }
 
 function make_axes(x1,y1,x2,y2){
@@ -54,7 +144,7 @@ function logistic(r,x_in,dt,x1,y1,x2,y2){
 
 	var x = x_in;
 	stroke(167,250,184)
-    strokeWeight(10);
+        strokeWeight(10);
 	point( t0, bottom-scale*x );
 	for(var t=t0; t<T; t+=dt )
 	{
@@ -69,21 +159,34 @@ function logistic(r,x_in,dt,x1,y1,x2,y2){
 	make_axes(x1,y1,x2,y2)
 }
 
+function draw_tent(x,y)
+{
+    push();
+    translate(x,y);
+    scale(0.85)
+    image(tent,0,0);
+    pop();
+}
+
 function draw() {
-  //var r = rSlider.value();
-  //var x0 = xSlider.value();
-  //var dt = tSlider.value();
-  background(100);
+    background(150);
+    noStroke();
+    textFont("sans",40);
+    text("Change the ratio between pots", 100, 55);
+    textFont("sans",30);
+    text("pot A / pot B",  245, 115);
 
-  noStroke();
-  textFont("sans",40);
-  text("Sample javascript slider", 100, 55);
-  textFont("sans",30);
-  text("slider",    245, 115);
+    tent_x = 40
+    tent_y = 135
+    draw_tent(tent_x, tent_y)
+    image(peep,60 + 0.2*column.height, 150+0.75*column.height, peep.width/2, peep.height/2)
 
-  //r = r/200.0
-  //x0 = x0/1000.0
-  //dt = dt
-  //logistic(r,x0,dt,50,225,width-100,height-80);
+    update_simulation();
+    draw_simulation(tent_x+0.2*tent.width, 0.65*tent.height)//tent_y+tent.height);
+
+    //r = r/200.0
+    //x0 = x0/1000.0
+    //dt = dt
+    //logistic(r,x0,dt,50,225,width-100,height-80);
 
 }
